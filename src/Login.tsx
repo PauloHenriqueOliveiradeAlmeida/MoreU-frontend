429e11b8d5d3eae5391ab6d533bb3bb2782c5950
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2023-09-08 13:39:24 -0300

    :sparkles:feat: request de dados do formulário, configuração do axios, preparo de rota de login (ainda não desenvolvida) e filtragem de dados do formulário

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,19 +1,53 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./assets/styles/forms.scss";
+import api from "./services/api";
+import Clientes from "./types/clientes";
 
 function Login() {
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: ""
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().email("Você deve digitar um e-mail válido").required("Esta informação é obrigatória"),
+            password: Yup.string().required("Esta informação é obrigatória")
+        }),
+        onSubmit: (values) => handleSubmit(values)
+    })
+
+    async function handleSubmit(data: Clientes) {
+        const request = await api.post("login", data);
+
+        return request.data;
+    }
+
+
     return (
         <div className="background">
             <form>
                 <h2>Faça seu Login</h2>
                 <div>
-                    <label htmlFor="">Nome:</label>
-                    <input type="text" />
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} required/>
+                    
+                    {formik.touched.email && formik.errors.email ? (
+                        <div className="message-error">{formik.errors.email}</div>
+                    ) : null}
+
                 </div>
                 
 
                 <div>
-                    <label htmlFor="">Senha:</label>
-                    <input type="text" />
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" id="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} required/>
+                    
+                    {formik.touched.password && formik.errors.password ? (
+                        <div className="message-error">{formik.errors.password}</div>
+                    ) : null}
+
                 </div>
 
                 <button type="submit"><span>Fazer Login</span></button>
---
+++ src/types/clientes.d.ts
@@ -0,0 +1,10 @@
+interface Clientes {
+    id?: number,
+    nome?: string,
+    email?: string,
+    password?: string,
+    contato?: string,
+    nomeEmpresa?: string
+}
+
+export default Clientes;
